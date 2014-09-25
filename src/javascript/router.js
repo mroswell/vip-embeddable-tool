74a2a4b7d0994795b4723955cd94ee29f2e08a3f
Alex Konrad <alexkonrad08@gmail.com>
2014-09-25 13:52:35 -0400

    extend

--- src/javascript/router.js
+++ src/javascript/router.js
@@ -5,27 +5,31 @@ module.exports = (function() {
     , apiRequest  = require('./api.js')
     , text        = require('./config.js')
     , $           = require('jquery');
+window.$ = $
 
   return {
-    start: function(options) {
+    start: function(config) {
       var router = this;
-      
-      var appOptions = {
-        modal : typeof options.modal !== 'undefined' ? options.modal : true,
-        officialOnly : typeof options.officialOnly !== 'undefined' ? options.officialOnly : true,
-        alert : typeof options.alert !== 'undefined' ? options.alert : null,
-        test : typeof options.test !== 'undefined' ? options.test : false,
-        key: typeof options.key !== 'undefined' ? options.key : 'AIzaSyCLNlhlWcKcozqYRq9M1_j25GLUzqrJxH8',
-        title : typeof options.title !== 'undefined' ? options.title : 'Voting Information Tool',
-        subtitle: typeof options.subtitle !== 'undefined' ? options.subtitle : '',
-        logo : typeof options.logo  !== 'undefined' ? options.logo : 'https://s3.amazonaws.com/vip-voter-information-tool/images/voting-information-project.png',
-        smallLogo : typeof options.smallLogo !== 'undefined' ? options.smallLogo : 'https://s3.amazonaws.com/vip-voter-information-tool/images/vip-logo.png',
-        language: typeof options.language !== 'undefined' ? options.language : 'en',
-        width : typeof options.width !== 'undefined' ? options.width : 640,
-        height : typeof options.height !== 'undefined' ? options.height : 480,
-        productionDataOnly: typeof options.productionOnly !== 'undefined' ? options.productionOnly : true,
-        assets : text
+
+      var options = {
+        modal: true,
+        officialOnly: false,
+        alert: null,
+        test: false,
+        key: 'AIzaSyCLNlhlWcKcozqYRq9M1_j25GLUzqrJxH8',
+        title: 'Voting Information Project',
+        subtitle: '',
+        logo: 'https://s3.amazonaws.com/vip-voter-information-tool/images/voting-information-project.png',
+        smallLogo: 'https://s3.amazonaws.com/vip-voter-information-tool/images/vip-logo.png',
+        language: 'en',
+        width: 640,
+        height: 480,
+        productionDataOnly: true,
+        assets: text
       };
+      $.extend(options, config);
+      
+
 
       addressView
         .onRouteEvent('addressViewSubmit', function(response) {
@@ -33,40 +37,40 @@ module.exports = (function() {
           window.console && console.log(data);
           window.history && history.pushState && history.pushState(null, null, '?polling-location');
           $(window).on('popstate', function() {
-            router.navigate(addressView, mapView, appOptions);
+            router.navigate(addressView, mapView, options);
             $('#_vitModal').hide();
           }.bind(this));
-          appOptions['data'] = data;
-          router.navigate(mapView, addressView, appOptions);
+          $.extend(options, { data: data })
+          router.navigate(mapView, addressView, options);
         });
 
       mapView
         .onRouteEvent('mapViewBack', function() {
           // if the user chose the election, reroute to the election choice view
           // else go back to the address entry view
-          router.navigate(addressView, mapView, appOptions);
+          router.navigate(addressView, mapView, options);
         })
         .onRouteEvent('mapViewRerender', function() {
-          appOptions['data'] = data;
-          router.navigate(mapView, mapView, appOptions)
+          $.extend(options, { data: data })
+          router.navigate(mapView, mapView, options)
         })
         .onRouteEvent('mapViewSubmit', function(response) {
           data = response;
-          appOptions['data'] = data;
+          $.extend(options, { data: data })
 
           // render the elections view if there's more than one election
           // returned for the entered address, otherwise render the map view
           if (typeof data.otherElections !== 'undefined') {
-            router.navigate(electionsView, mapView, appOptions);
-          } else router.navigate(mapView, mapView, appOptions);
+            router.navigate(electionsView, mapView, options);
+          } else router.navigate(mapView, mapView, options);
         });
 
       if (options.json) {
         $.getJSON(options.json, function(newText) {
-            appOptions['assets'] = newText;
-            addressView.render(appOptions);
+          $.extend(options, { assets: newText });
+          addressView.render(options);
         });
-      } else addressView.render(appOptions);
+      } else addressView.render(options);
     },
 
     navigate: function(toView, fromView, options) {
